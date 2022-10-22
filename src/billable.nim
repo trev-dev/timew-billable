4c6b33542ce5908848bd43b686d07a35ea326a1f
Trevor Richards <trev@trevdev.ca>
2022-10-22 09:18:36 -0700

    finish config parser

--- src/billable.nim
+++ src/billable.nim
@@ -1,19 +1,18 @@
-import std/strutils
-import std/re
-import std/sequtils
+import std/[strutils, re, sequtils]
 
 type
-  ClientSpecificRate = tuple[client: string, rate: int]
-  Config = object
+  ClientSpecificRate = tuple[client: string, rate: float]
+  Config = tuple
     projectMarker: string
-    billable: int
+    billable: float
     clients: seq[ClientSpecificRate]
     descriptionMarker: string
     locale: string
 
 const CONFIG_DEFAULTS = Config(
+const CONFIG_DEFAULTS: Config = (
   projectMarker: "#",
-  billable: 0,
+  billable: 0.0,
   clients: @[],
   descriptionMarker:"",
   locale: ""
@@ -21,16 +20,37 @@ const CONFIG_DEFAULTS = Config(
 
 func stripStr(s: string): string = strip s
 
-proc createConfigObject(keys: seq[string]): Config =
-  var conf: Config
-  for i in keys:
-    let kvpair = map(split(i, ":"), proc (s:string):string = strip s)
-    let conf_keys = kvpair[0].split('.')
-    if conf_keys[0] != "billable": continue
+func coerceFloat(s:string): float =
+  try:
+    return parseFloat(s)
+  except ValueError as e:
+    assert(
+      false,
+      "Failed to parse config! Value " & s & " should be a float type."
+    )
 
+proc createConfig(keys: seq[string]): Config =
+  var conf = CONFIG_DEFAULTS
+  for i in keys:
+    let kvpair = map(split(i, ":", 1), proc (s:string):string = strip s)
+    if kvpair[1] == "": continue
+
+    let conf_keys = split(kvpair[0], ".", 1)
+    if len(conf_keys) == 1:
+      conf.billable = coerceFloat(kvpair[1])
+
+    else:
+      case conf_keys[1]:
+        of "project_marker": conf.projectMarker = kvpair[1]
+        of "description_marker": conf.descriptionMarker = kvpair[1]
+        of "locale": conf.locale = kvpair[1]
+        else:
+          let rate = coerceFloat(kvpair[1])
+          add(conf.clients, (client: conf_keys[1], rate: rate))
+  return conf
 
 let data = split(readAll(stdin), "\n\n")
 let headerKeys = findAll(data[0], re"(^|\n)billable.*")
 let strippedKeys = map(headerKeys, proc (s: string):string = strip s)
+let config = createConfig(strippedKeys)
 
-discard createConfigObject(strippedKeys)
