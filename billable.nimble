44107ab1b7a12025af36331eae78e686950e04be
Trevor Richards <trev@trevdev.ca>
2022-10-29 08:23:08 -0700

    poop

---
+++ billable.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.0.0"
+author        = "TrevDev"
+description   = "timew-billable"
+license       = "MIT"
+srcDir        = "src"
+
+# Dependencies
+
+requires "nim >= 1.6.6"
+requires "jsony"
--- src/billable.nim
+++ src/billable.nim
@@ -1,4 +1,5 @@
 import std/[strutils, re, sequtils]
+import jsony
 
 type
   ClientSpecificRate = tuple[client: string, rate: float]
@@ -7,14 +8,12 @@ type
     billable: float
     clients: seq[ClientSpecificRate]
 
-const CONFIG_DEFAULTS = Config(
-const CONFIG_DEFAULTS: Config = (
-  projectMarker: "#",
-  billable: 0.0,
-  clients: @[],
-  descriptionMarker:"",
-  locale: ""
-)
+  RawTimeEntry =
+    tuple[id: int, start: string, `end`: string, tags: seq[string]]
+  RawTimewEntries = seq[RawTimeEntry]
+
+  TimeEntry = tuple[name: string, hours: float, cost: float, rate: float]
+  Table = seq[seq[TimeEntry]]
 
 func coerceFloat(s:string): float =
   try:
@@ -25,8 +24,10 @@ func coerceFloat(s:string): float =
       "Failed to parse config! Value " & s & " should be a float type."
     )
 
-proc createConfig(keys: seq[string]): Config =
-  var conf = CONFIG_DEFAULTS
+func createConfig(keys: seq[string]): Config =
+  var conf: Config
+  conf.projectMarker = "#"
+
   for i in keys:
     let kvpair = map(split(i, ":", 1), proc (s:string):string = strip s)
     if kvpair[1] == "": continue
@@ -36,15 +37,46 @@ proc createConfig(keys: seq[string]): Config =
       conf.billable = coerceFloat(kvpair[1])
 
     else:
-      case conf_keys[1]:
+      case conf_keys[1]
         of "project_marker": conf.projectMarker = kvpair[1]
         else:
           let rate = coerceFloat(kvpair[1])
           add(conf.clients, (client: conf_keys[1], rate: rate))
   return conf
 
-let data = split(readAll(stdin), "\n\n")
-let headerKeys = findAll(data[0], re"(^|\n)billable.*")
-let strippedKeys = map(headerKeys, proc (s: string):string = strip s)
-let config = createConfig(strippedKeys)
+func parseEntryHierarchy(tags: seq[string], pMarker: string): seq[string] =
+  # TODO: Maybe validate taskName based on whitespace
+  if tags[0].startsWith pMarker:
+    let projectHierarchy = tags[0][pMarker.len..^1].split "."
+    let taskName = tags[1]
+    result = projectHierarchy.concat @[taskName]
+  else:
+    result.add tags[0]
+
+func updateTableRow(
+  table: Table, hierarchy: seq[string], entry: RawTimeEntry
+): seq[TimeEntry] =
+  let levels = hierarchy.len
+
+func prepareTableData(config: Config, rawEntries: RawTimewEntries): Table =
+  var table: Table = @[]
+  for i in 0 ..< rawEntries.len:
+    let entry = rawEntries[i]
+    let hierarchy = entry.tags.parseEntryHierarchy config.projectMarker
+
+  return table
+
+func stripString(s:string):string = s.strip()
+
+let rawConfigAndEntries = readAll(stdin).split("\n\n")
+
+let configStrings = rawConfigAndEntries[0]
+  .findAll(re"(^|\n)billable.*")
+  .map(stripString)
+
+let config = createConfig(configStrings)
+
+let jsonData = rawConfigAndEntries[1].fromJson(RawTimewEntries)
+
+discard config.prepareTableData jsonData
 
